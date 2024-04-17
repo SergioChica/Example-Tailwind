393beec47c651f0613216ad3be19c563ec38ac7e
Sergio Chica <sergioanjaci@gmail.com>
2024-04-16 23:23:10 -0500

    Add js

---
+++ src/array.js
@@ -0,0 +1,23 @@
+const object = [
+    {
+        id: 1,
+        img: hola.png,
+        date: "24-04-2005",
+        description: "Example 1",
+    },{
+        id: 2,
+        img: hello.png,
+        date: "27-08-2005",
+        description: "Example 2",
+    },{
+        id: 3,
+        img: adios.png,
+        date: "26-08-2001",
+        description: "Example 3",
+    },{
+        id: 4,
+        img: bye.png,
+        date: "30-11-2020",
+        description: "Example 4",
+    },
+]
