5af28b80f41adbba7400a1fa28c7fb1f64411979
sergio chica <sergioanjaci@gmail.com>
2024-04-24 16:43:47 -0500

    Finish page

--- src/components/Layouts/Main/Main.jsx
+++ src/components/Layouts/Main/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Main = ({children}) => {
   return (
-    <main className='h-full bg-neutral-950'>
+    <main className='min-h-full h-auto flex flex-col gap-[30px] bg-neutral-950 pb-[40px] '>
       {children}
     </main>
   )
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HeaderBlog } from '../../HeaderBlog/HeaderBlog'
 import { Main } from '../../Layouts/Main/Main'
 import { FirstNew } from '../../FirstNew/FirstNew'
-import { object } from '../../Object/Object'
+import { object,slice } from '../../Object/Object'
 import { AllNews } from '../../AllNews/AllNews'
 
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
+import { RxDotFilled } from 'react-icons/rx'
+
 export const Home = () => {
+
+  const [viewImg, setViewImg] = useState(0);
+
+  const prevSlide = () => {
+    const firstSlide = viewImg === 1;
+    const newImg = firstSlide ? object.length - 1 : viewImg - 1;
+    setViewImg(newImg);
+  }
+
+  const nextSlide = () => {
+    const lastSlide = viewImg === object.length - 1;
+    const newImg = lastSlide ? 0 : viewImg + 1;
+    setViewImg(newImg);
+  }
+
+  const goToSlide = (numberImg) => {
+      setViewImg(numberImg);
+  }
 
   return (
     <>
@@ -37,12 +58,28 @@ export const Home = () => {
             )).splice(1, 3)}
           </section>
         </div>
-        <div>
-            <div>
-              <img src="" alt="" />
-              <img src="" alt="" />
-              <img src="" alt="" />
+        <div className='w-full h-full flex justify-center'>
+          <div className='w-[800px] h-[400px] relative group'>
+            <div className='w-[800px] h-[400px]'>
+              <div style={{
+                backgroundImage: `url(${slice[viewImg].img})`
+              }} className=' w-full h-full rounded-[26px] bg-center bg-cover duration-500'></div>
             </div>
+
+            <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 rounded-full p-2 bg-black/20 text-white cursor-pointer '>
+              <MdArrowBackIos onClick={prevSlide} size={30} />
+            </div>
+            <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 rounded-full p-2 bg-black/20 text-white cursor-pointer  '>
+              <MdArrowForwardIos onClick={nextSlide} size={30} />
+            </div>
+            <div className='flex top-4  justify-center py-2 '>
+              {slice.map((img, numberImg) => (
+                <div key={numberImg} onClick={() => goToSlide(numberImg)} className=' text-[18px] cursor-pointer'>
+                  <RxDotFilled color='white' />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </Main>
     </>
